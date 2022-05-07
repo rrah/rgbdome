080f39ecd1f297411ed6a24a710c381ba31ad74c
Robert Walker <rrah99@gmail.com>
2022-05-07 20:28:27 +0100

    start of arduino_debug removal

--- arduino/stm32-setup.py
+++ arduino/stm32-setup.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import time
 
 parser = argparse.ArgumentParser(description="Program a controller.")
 parser.add_argument("id", type=int, help="ID number of controller")
@@ -17,6 +18,25 @@ def_mac = "#define MAC {{0x{:02x},0x{:02x},0x{:02x},0x{:02x},0x{:02x},0x{:02x}}}
 
 stlink_exe = "C:\\Program Files (x86)\\Arduino\\hardware\\Arduino_STM32-master\\tools\\win\\stlink\\ST-LINK_CLI.exe"
 stlink_args = ["-c", "SWD", "-P", "generic_boot20_pc13.bin", "0x8000000", "-Rst", "-Run"]
+
+build_exe = "C:\\Program Files (x86)\Arduino\\arduino-builder"
+build_args = [
+    "-logger=machine", 
+    "-hardware", "C:\\Program Files (x86)\\Arduino\\hardware", 
+    "-hardware", "C:\\Users\\redsw\\AppData\\Local\\Arduino15\\packages", 
+    "-hardware", "C:\\Users\\redsw\\OneDrive\\Documents\\Arduino\\hardware", 
+    "-tools", "C:\\Program Files (x86)\\Arduino\\tools-builder", 
+    "-tools", "C:\\Program Files (x86)\\Arduino\\hardware\\tools\\avr",
+    "-tools", "C:\\Users\\redsw\\AppData\\Local\\Arduino15\\packages",
+    "-built-in-libraries", "C:\\Program Files (x86)\\Arduino\\libraries",
+    "-libraries", "C:\\Users\\redsw\\OneDrive\\Documents\\Arduino\\libraries",
+    "-fqbn=Arduino_STM32-master:STM32F1:genericSTM32F103C:device_variant=STM32F103C8,upload_method=STLinkMethod,cpu_speed=speed_72mhz,opt=osstd",
+    "-ide-version=10812",
+    "-build-path", "C:\\Workspace\\rgbdome\\arduino\\temp\\arduino-build",
+    "-warnings=none", 
+    "-build-cache", "C:\\Users\\redsw\\AppData\\Local\\Temp\\arduino_cache_342428", 
+    "-prefs=build.warn_data_percentage=75"
+    ]
 
 arduino_exe = "C:\\Program Files (x86)\\Arduino\\arduino_debug.exe"
 arduino_args = ["--upload"]
@@ -41,6 +61,8 @@ input("Move boot0 to 0...")
 ## Flash eeprom
 args = [arduino_exe] + arduino_args + [arduino_file]
 subprocess.run(args)
+
+time.sleep(3)
 
 ## Flash final script
 args = [arduino_exe] + arduino_args + [arduino_final]
